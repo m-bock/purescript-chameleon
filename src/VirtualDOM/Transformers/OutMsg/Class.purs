92a81d8149733bc17f697a1666a9b2b61e7870a3
Michael Bock <no-day@posteo.net>
2023-07-30 18:19:29 +0200

    Remove transformers

--- src/VirtualDOM/Transformers/OutMsg/Class.purs
+++
@@ -1,9 +0,0 @@
-module VirtualDOM.Transformers.OutMsg.Class where
-
-import Data.These (These)
-
-class OutMsg out html | html -> out where
-  fromOutHtml :: forall a. html (These a out) -> html a
-
-class OutMsg out html <= RunOutMsg out html | html -> out where
-  runOutMsg :: forall msg. html msg -> html (These msg out)
